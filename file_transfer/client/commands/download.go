3d76278b80b5d76f39dcdb54e88fff78b51bf789
Maria <m.skakalina@yandex.ru>
2024-06-12 15:13:24 +0300

    Download first

--- file_transfer/client/commands/download.go
+++ file_transfer/client/commands/download.go
@@ -1,11 +1,64 @@
 package commands
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"os"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 func Download(conn net.Conn) {
-	fmt.Println(conn)
-	fmt.Println("Hello download")
+	reader, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		log.Println(err)
+	}
+
+	id := uuid.New()
+
+	fmt.Print(reader)
+	readerName := bufio.NewReader(os.Stdin)
+	nameFile, err := readerName.ReadString('\n')
+	if err != nil {
+		log.Println("Ошибка чтения имени файла: ", err)
+	}
+	nameFile = strings.TrimSpace(nameFile)
+	fmt.Fprintln(conn, nameFile)
+	// readerResponce := bufio.NewReader(os.Stdin)
+	// responce, err := readerResponce.ReadString('\n')
+	// if err != nil {
+	// 	log.Println("Ошибка чтения имени файла: ", err)
+	// }
+
+	_, err = os.Stat("uploads")
+	if os.IsNotExist(err) {
+		err := os.Mkdir("uploads", os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	filePath := fmt.Sprintf("uploads/%s.jpeg", id.String())
+	file, err := os.Create(filePath)
+	if err != nil {
+		log.Println("Ошибка при создании файла: ", err)
+	}
+	defer file.Close()
+	done := make(chan error)
+
+	writer1 := bufio.NewWriter(file)
+	reader1 := bufio.NewReader(conn)
+	go func() {
+		_, err := io.CopyN(writer1, reader1, 121978)
+		done <- err
+	}()
+	err = <-done
+	if err != nil && err != io.EOF {
+		fmt.Println("Error during copy:", err)
+	}
+
 }
--- file_transfer/server/commander/download.go
+++ file_transfer/server/commander/download.go
@@ -1,11 +1,62 @@
 package commander
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"os"
+	"strings"
 )
 
 func Download(conn net.Conn) {
-	fmt.Println(conn)
-	fmt.Println("Hello download")
+	text := []byte("Выберите файл для загрузки: \n")
+	if _, err := conn.Write(text); err != nil {
+		log.Println(err)
+	}
+	reader, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		log.Println(err)
+	}
+	fileName := strings.TrimSpace(reader)
+	file, err := ReadFiles()
+	if err != nil {
+		log.Println(err)
+	}
+	for _, v := range file.Files {
+		if v.Name == fileName {
+			file, err := os.Open(v.Path)
+			if err != nil {
+				log.Println(err)
+			}
+			defer file.Close()
+			size, _ := file.Stat()
+			sizeFile := size.Size()
+
+			reader := bufio.NewReader(file)
+			buffer := make([]byte, sizeFile)
+
+			for {
+				n, err := reader.Read(buffer)
+				fmt.Println(n)
+				if err != nil {
+					if err == io.EOF {
+						break // Конец файла
+					}
+					log.Println("Ошибка чтения куска файла: ", err)
+					os.Exit(1)
+				}
+				_, err = conn.Write(buffer[:n])
+				if err != nil {
+					log.Println("Ошибка записи на сервер: ", err)
+				}
+			}
+		} else {
+			if _, err := conn.Write([]byte("Такого файла нет, посмотрите список доступных файлов с помощью команды \"all_files\"\n")); err != nil {
+				log.Println(err)
+			}
+			return
+		}
+	}
 }
