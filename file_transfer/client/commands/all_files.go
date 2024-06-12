57e41e053a4975cb577d4d37c067fe3ac1ce2dca
Maria <m.skakalina@yandex.ru>
2024-06-12 14:09:31 +0300

    All files

--- file_transfer/client/commands/all_files.go
+++ file_transfer/client/commands/all_files.go
@@ -1,35 +1,49 @@
 package commands
 
 import (
-	"bufio"
-	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+
+	"github.com/google/uuid"
 )
 
+type Files struct {
+	Files []File `json:"files"`
+}
+
+type File struct {
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+	Path string    `json:"path"`
+}
+
 func AllFiles(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-	for {
-		// Чтение размера файла
-		var sizeFile uint32
-		if err := binary.Read(reader, binary.LittleEndian, &sizeFile); err != nil {
-			if err == io.EOF {
-				break // Выходим из цикла, если соединение закрыто
-			}
-			log.Println("Ошибка чтения размера файла:", err)
-			return
-		}
-
-		// Чтение имени файла до символа новой строки
-		fileName := make([]byte, sizeFile)
-		if _, err := io.ReadFull(reader, fileName); err != nil {
-			log.Println("Ошибка чтения имени файла:", err)
-			return
-		}
-
-		// Вывод имени файла
-		fmt.Println(string(fileName))
+	var files Files
+	file, err := os.Open("../files.json")
+	if err != nil {
+		log.Println(err)
 	}
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if len(byteValue) == 0 {
+		log.Println(err)
+	}
+
+	if err = json.Unmarshal(byteValue, &files); err != nil {
+		log.Println(err)
+	}
+
+	for _, v := range files.Files {
+		fmt.Println(v.Name)
+	}
+
 }
--- file_transfer/server/commander/all_files.go
+++ file_transfer/server/commander/all_files.go
@@ -1,33 +1,41 @@
 package commander
 
 import (
-	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func AllFiles(conn net.Conn) {
-	files, err := ReadFiles()
+	var files Files
+	file, err := os.Open("../files.json")
 	if err != nil {
+		log.Println("Ошибка открытия файла:", err)
+		return
+	}
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		log.Println("Ошибка чтения файла:", err)
+	}
+	if err = json.Unmarshal(byteValue, &files); err != nil {
 		log.Fatal(err)
 	}
-
-	for _, file := range files.Files {
-		fmt.Println(file.Name)
-		buffer := []byte(file.Name + "\n")
-
-		// Проверка размера буфера перед отправкой
-		bufferSize := len(buffer)
-
-		if err := binary.Write(conn, binary.LittleEndian, uint32(bufferSize)); err != nil {
-			log.Println("Ошибка записи размера буфера: ", err)
-			return // Если произошла ошибка, выходим из функции
-		}
-
-		if _, err := conn.Write(buffer); err != nil {
-			log.Println("Ошибка записи имени файла: ", err)
-			return // Если произошла ошибка, выходим из функции
-		}
+	byteFiles, err := json.Marshal(files)
+	if err != nil {
+		log.Println(err)
 	}
+	for _, v := range files.Files {
+		fmt.Println(v.Name)
+	}
+	fmt.Println(string(byteFiles))
+
+	// // Отправляем данные клиенту
+	// if _, err := conn.Write(byteFiles); err != nil {
+	// 	log.Println("Ошибка отправки данных:", err)
+	// }
 }
